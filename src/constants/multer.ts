f12b156be9c62e196ff5fefaf03aa6b5dfe08f3d
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-05-05 15:40:46 +0300

    implemented multer configuration

---
+++ src/constants/multer.ts
@@ -0,0 +1,36 @@
+import multer from 'multer'
+import { MAX_CONTAINER_SIZE_MB } from './config'
+import * as fs from 'fs'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const destinationFolder = req.params.id || 'default'
+
+    const uploadDir = `uploads/${destinationFolder}`
+
+    fs.mkdir(uploadDir, { recursive: true }, (err: NodeJS.ErrnoException | null) => {
+      if (err) return cb(err, uploadDir)
+
+      cb(null, uploadDir)
+    })
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.webp')
+  },
+})
+
+export const upload = multer({
+  dest: 'uploads/',
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith('image/')) {
+      return cb(new Error('File must be an image'))
+    }
+    cb(null, true)
+  },
+
+  limits: {
+    fileSize: MAX_CONTAINER_SIZE_MB * 1024 * 1024,
+  },
+})
