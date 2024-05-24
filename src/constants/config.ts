855415e10db17d9ff755790005cf5432074e265f
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-05-24 14:38:21 +0300

    update DB name & limits

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,12 +2,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const EXPRESS_PORT = process.env.PORT || 8001
-export const MONGO_DBNAME = 'Xauto'
+export const MONGO_DBNAME = 'Garaaz24'
 
 export const IMAGE_CONTAINER_COLLETION = 'image-containers'
 
-export const MAX_CONTAINER_SIZE_MB = 200
-export const MAX_CONTAINER_FILES = 500
+export const MAX_CONTAINER_SIZE_MB = 30
+export const MAX_CONTAINER_FILES = 30
 
 export const INTERNAL_SECRET_API_KEY = process.env.INTERNAL_API_KEY
 export const CONTAINER_UPDATE_PING_URL = process.env.CONTAINER_UPDATE_PING_URL
